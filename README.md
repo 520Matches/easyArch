5dc3beba540d4bbb079a254b58e36019f6446f73
520Matches <HengDi_Sun@163.com>
2020-09-28 13:43:25 +0000

    add install brash

---
+++ README.md
@@ -0,0 +1,30 @@
+- fork https://github.com/S0ulf7y/easyarch
+# easy install arch linux
+
+## Installation
+
+1. Boot from an Arch ISO
+2. Make sure your computer is connected to the internet
+3. Activate the german keyboard layout:
+  ```
+  loadkeys de
+  ```
+4. Update pacman and install git:
+  ```
+  pacman -Sy && pacman -S git --noconfirm
+  ```
+5. Clone the script to your machine:
+  ```
+  git clone https://github.com/S0ulf7y/easyarch.github
+  ```
+6. Make both scripts executable:
+  ```
+  cd easyarch/
+  chmod +x install.sh install2.sh
+  ```
+7. Run the install script:
+  ```
+  ./install.sh
+  ```
+8. Follow the the instructions of the installer
+9. Enjoy your fresh Arch Linux installation!
---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# This script is intendet to install a simple and lightweight instance of Arch Linux on Desktop and Notebook systems.
+# The installation contains a setup for german language user expirience.
+# A working internet connection is required.
+# The installation process is splitted in two scripts. This is script 1/2 for the installation of the base system. Script 2/2 will be copied to the hard drive and executed automatically.
+# TODO: Setup Full Disk Encryption option
+
+# Quit the script if any executed command fails:
+set -e
+
+# Set german keyboard layout
+#loadkeys de
+
+# Select the disk for the installation
+echo "On which disk do you want to install Arch Linux?"
+echo 'You can just type i.e. "sda"'
+lsblk
+read -p "Disk:" disk
+
+# Wipe the disk
+sgdisk /dev/"$disk" -o
+# Create the partition table
+echo "Creating GPT table..."
+parted /dev/"$disk" mklabel gpt --script
+echo "Success!"
+
+# Create the boot partition (always 512MB)
+echo "Creating boot partition..."
+parted /dev/"$disk" mkpart BOOT fat32 1MiB 513MiB
+echo "Success!"
+
+# Create the root partiton dynamically after the Swap partition
+echo "Creating root partition..."
+parted /dev/"$disk" mkpart p_arch ext4 514MiB 100%
+echo "Success!"
+
+# Set the boot disk as an EFI device
+echo "Setting /dev/"$disk" as EFI device..."
+parted /dev/"$disk" set 1 esp on
+echo "Success!"
+
+# Create the file systems for the new partitions
+mkfs.ext4 -L p_arch /dev/"$disk"2
+mkfs.fat -F 32 -n BOOT /dev/"$disk"1
+
+# Mount the newly created partitons
+mount /dev/"$disk"2 /mnt
+mkdir /mnt/boot
+mount /dev/"$disk"1 /mnt/boot
+
+# Configure and update the pacman index
+#cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+#grep -E -A 1 ".*Germany.*$" /etc/pacman.d/mirrorlist.bak | sed '/--/d' > /etc/pacman.d/mirrorlist
+pacman -Sy
+
+# Install the base system
+pacstrap /mnt base base-devel linux linux-firmware wpa_supplicant grub acpid xorg xorg-drivers alsa-utils pulseaudio pulseaudio-alsa wireless_tools networkmanager network-manager-applet sudo linux-headers dosfstools efibootmgr slock 
+
+# Automatically generate the fstab file from the mount configuration
+genfstab -Lp /mnt > /mnt/etc/fstab
+
+# Copy the second file for the advanced config process to the hdd
+cp ./install2.sh /mnt
+
+# Copy the files for the X11 locale setup to the hdd
+#cp ./setx11locale.service /mnt/etc/systemd/system
+#cp ./setx11locale /mnt/usr/bin
+
+# Switch to the newly installed system and run the second file
+arch-chroot /mnt /install2.sh
